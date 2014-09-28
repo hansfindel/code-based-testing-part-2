82d86ef4fcee66c44686ae38be36a201838dee28
Guillermo Valenzuela <gevalenz@gmail.com>
2014-09-28 14:23:02 -0300

    Adding max_health to code_name_factory

--- spec/factories/code_name_factory.rb
+++ spec/factories/code_name_factory.rb
@@ -4,24 +4,28 @@ FactoryGirl.define do
         name
         damage 5
         info_reference "wiki"
+        max_health 100
     end
 
     factory :T_800, class: CodeName do
         name "T-800"
         damage 6
         info_reference "http://en.wikipedia.org/wiki/Terminator_(character)"
+        max_health 100
     end
 
     factory :T_1000, class: CodeName do
         name "T-1000"
         damage 10
         info_reference "http://en.wikipedia.org/wiki/T-1000"
+        max_health 100
     end
 
     factory :T_X, class: CodeName do
         name "T-X"
         damage 15
         info_reference "http://en.wikipedia.org/wiki/T-X"
+        max_health 100
     end
 
 end
