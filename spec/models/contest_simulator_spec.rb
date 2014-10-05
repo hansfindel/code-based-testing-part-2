09a187235a2f89d8b380e7fd8741678786f34608
Carl <cnschroter@uc.cl>
2014-10-05 08:59:00 -0300

    fixed simulator spec

--- spec/models/contest_simulator_spec.rb
+++ spec/models/contest_simulator_spec.rb
@@ -5,11 +5,12 @@ require 'rails_helper'
     context "1vs1 simultaneous attack (test)" do
       before(:each) do
         @r1 = FactoryGirl.create :robot #damage = 5, health = 50
+        @r3 = FactoryGirl.create :robot #damage = 5, health = 50
         @r2 = FactoryGirl.create :t_800 #damage = 10, health = 80
       end
       it "should be a 'Tragic Tie'" do
         # empty array means tie
-        expect(ContestSimulator.test(@r1,@r1)).to match_array([])
+        expect(ContestSimulator.test(@r1,@r3)).to match_array([])
       end
       it "should win contender1" do
         result = ContestSimulator.test(@r2,@r1)
