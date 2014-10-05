ac64942cca25aed4e28e56ad2d8bd8bff3ff444a
javicabello <jacabell@uc.cl>
2014-10-05 10:31:52 -0300

    Agrego tests

--- spec/factories/code_name_factory.rb
+++ spec/factories/code_name_factory.rb
@@ -3,24 +3,28 @@ FactoryGirl.define do
     factory :code_name do
         name
         damage 5
+        technology 5
         info_reference "wiki"
     end
 
     factory :T_800, class: CodeName do
         name "T-800"
         damage 6
+        technology 6
         info_reference "http://en.wikipedia.org/wiki/Terminator_(character)"
     end
 
     factory :T_1000, class: CodeName do
         name "T-1000"
         damage 10
+        technology 10
         info_reference "http://en.wikipedia.org/wiki/T-1000"
     end
 
     factory :T_X, class: CodeName do
         name "T-X"
         damage 15
+        technology 15
         info_reference "http://en.wikipedia.org/wiki/T-X"
     end
 
--- spec/factories/weapon_factory.rb
+++ spec/factories/weapon_factory.rb
@@ -3,21 +3,25 @@ FactoryGirl.define do
     factory :gun, class: Weapon do
         name "Gun"
         damage 5
+        technology 5
     end
 
     factory :rifle, class: Weapon do
         name "Rifle"
         damage 10
+        technology 10
     end
 
     factory :machine_gun, class: Weapon do
         name "Machine Gun"
         damage 20
+        technology 20
     end
 
     factory :bazuka, class: Weapon do
         name "Bazuka"
         damage 70
+        technology 70
     end
 
 end
